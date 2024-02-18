a04f1fccd6d0df9d320aaf951a4e442404600325
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2024-02-19 00:07:10 +0600

    feat: fetch logs using convex in public page

--- app/public/page.tsx
+++ app/public/page.tsx
@@ -1,11 +1,25 @@
+"use client";
 import StatBadges from "./components/StatBadges";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 
 export default function PublicPage() {
+  const logs = useQuery(api.logs.list);
+
   return (
     <div className="space-y-6">
       <StatBadges />
       <div>
-        <h2 className="text-white">So far in 2024</h2>
+        <h2 className="text-white">{logs?.map((log) => (
+          <div key={log._id}>
+            <div>
+              {log.title}
+            </div>
+            <div>
+              {log.body}
+            </div>
+          </div>
+        ))}</h2>
       </div>
     </div>
   );
