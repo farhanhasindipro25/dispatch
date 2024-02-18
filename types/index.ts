246d547f7671509a93aab5bfd765b514ce8504bb
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2024-02-19 00:06:00 +0600

    feat: enum types for convex schema

---
+++ types/index.ts
@@ -0,0 +1,12 @@
+export enum LogType {
+    Blog = 0,
+    Socials,
+    Achievements,
+    Work,
+    Projects,
+    Log
+  }
+export enum BadgeType {
+    Active = "green",
+    Inactive = "red"
+  }
