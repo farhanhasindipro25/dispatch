76753275ae8c270871f0cf50e757c7348eac482e
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-02-19 22:17:31 +0600

    fix: Fixed merge conflict

--- app/public/components/ui/MonthlyTimeline.tsx
+++ app/public/components/ui/MonthlyTimeline.tsx
@@ -1,37 +1,46 @@
 "use client";
 import { api } from "@/convex/_generated/api";
 import { useQuery } from "convex/react";
+import { generateActivityLogMsg } from "@/app/common/helpers/UtilsKit"; // Import your utility function
+import monthlyTimelineData from "@/app/common/staticData/monthlyTimelineData";
 
 export default function MonthlyTimeline() {
   const logs = useQuery(api.logs.list);
+
   return (
     <div>
       <h2 className="text-neutral-400 underline font-semibold text-sm">
         SO FAR THIS YEAR
       </h2>
       <div className="divide-y divide-neutral-700 space-y-6">
-        {logs?.map((log) => (
-          <div className="pt-6 space-y-4">
-            <h2 className="text-white font-semibold text-base">
-              {new Date(log._creationTime).toLocaleDateString()}
-            </h2>
+        {monthlyTimelineData?.map((log) => (
+          <div key={log.month} className="pt-6 space-y-4">
+            <h2 className="text-white font-semibold text-base">{log.month}</h2>
             <div className="space-y-4 divide-y divide-neutral-800">
-
-              {timeline.activityLog?.map((log) => (
-                <div className="flex items-start gap-2 pt-4">
+              {log.activityLog?.map((activity) => (
+                <div
+                  key={activity.topic}
+                  className="flex items-start gap-2 pt-4"
+                >
                   <div>🚀</div>
                   <div className="space-y-2">
                     <p className="text-neutral-300 text-sm font-semibold">
-                      {generateActivityLogMsg(log.kind, log.topic).synonym}{" "}
-                      {generateActivityLogMsg(log.kind, log.topic).topic}.
+                      {
+                        generateActivityLogMsg(activity.kind, activity.topic)
+                          .synonym
+                      }{" "}
+                      {
+                        generateActivityLogMsg(activity.kind, activity.topic)
+                          .topic
+                      }
+                      .
                     </p>
                     <p className="text-neutral-400 text-sm font-medium">
-                      {log.thoughts}
+                      {activity.thoughts}
                     </p>
                   </div>
-
                 </div>
-              </div>
+              ))}
             </div>
           </div>
         ))}
