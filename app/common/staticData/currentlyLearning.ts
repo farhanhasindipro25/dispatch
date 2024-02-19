6a56d7647224cf18c7983e334ed99043a15f74a8
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-02-19 14:09:32 +0600

    Feat: Added currently fighting for section in feats page

---
+++ app/common/staticData/currentlyLearning.ts
@@ -0,0 +1,29 @@
+export const currentLearning = [
+  {
+    item: "Backend Best Practices.",
+  },
+  {
+    item: "Learn and perfect design patterns and system design",
+  },
+  {
+    item: "ExpressJS, MongoDB, Mongoose, PostGreSQL, Prisma",
+  },
+  {
+    item: "Alternative ways of form validation with React Hook Forms and Zod, as I have already explored Formik and YUP.",
+  },
+  {
+    item: "Efficient state management using Redux.",
+  },
+  {
+    item: "Learn and perfect Typescript for both Frontend and Backend",
+  },
+  {
+    item: "Explore VueJS",
+  },
+  {
+    item: "Animations using FramerMotion and GSAP",
+  },
+  {
+    item: "Try out ThreeJS",
+  },
+];
--- app/public/feats/page.tsx
+++ app/public/feats/page.tsx
@@ -1,5 +1,6 @@
 import { byteSizeVictories } from "@/app/common/staticData/byteSizeVitories";
 import Skills from "./skills/Skills";
+import { currentLearning } from "@/app/common/staticData/currentlyLearning";
 
 function Feats() {
   return (
@@ -19,10 +20,20 @@ function Feats() {
           ))}
         </div>
       </div>
-      <div className="pt-6">
+      <div className="pt-6 space-y-2">
         <h2 className="text-base text-neutral-300 font-semibold">
-          _currentlyFightingWith
+          _currentlyFightingFor
         </h2>
+        <div className="pl-6">
+          {currentLearning?.map((item) => (
+            <h2
+              className="list-item text-neutral-400 text-sm font-medium"
+              key={item.item}
+            >
+              {item.item}
+            </h2>
+          ))}
+        </div>
       </div>
       <Skills />
     </div>
