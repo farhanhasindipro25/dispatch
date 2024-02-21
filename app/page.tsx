3d0de595b959762b3569b060353440f6888e35a0
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-02-22 01:04:20 +0600

    feat: Added temporary landing page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,22 @@ import React from "react";
 
 export default function Home() {
   return (
-    <main>
-      <Link href="/public">Public Page</Link>
-    </main>
+    <div className="bg-neutral-950 min-h-screen">
+      <div className="flex items-center justify-center min-h-screen max-w-2xl mx-auto px-4 flex-col">
+        <h2 className="text-neutral-400 text-center font-medium text-lg">
+          <span className="italic font-semibold">DISPATCH</span> is an
+          innovative project being done for a hackathon. It's{" "}
+          <span className="italic font-semibold">GAMECHANGING</span> version
+          will be coming to life soon. Meanwhile, take a look at my{" "}
+          <Link
+            href="/public"
+            className="text-neutral-300 hover:text-white hover:underline"
+          >
+            Portfolio.{" "}
+          </Link>
+          (It won't make sense now, but I really needed a new portfolio! 😔)
+        </h2>
+      </div>
+    </div>
   );
 }
