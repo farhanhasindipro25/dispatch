df318020b2e8bc4f65bc6f64b274daa45d23fbe1
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-02-23 12:04:14 +0600

    fix: removed twitter from socials

--- app/common/staticData/onTheWeb.ts
+++ app/common/staticData/onTheWeb.ts
@@ -5,7 +5,7 @@ export const onTheWeb = [
     linkedin: "https://www.linkedin.com/in/farhan-hasin-dipro/",
     github: "https://github.com/farhanhasindipro25",
     facebook: "https://www.facebook.com/dipro.farhanhasin",
-    twitter: "https://www.facebook.com/dipro.farhanhasin",
+    twitter: "",
     hashnode: "https://farhanhasindipro25.hashnode.dev/",
     medium: "",
     personal_portfolio: "https://portfolio-farhan-hasin-dipro.web.app/",
