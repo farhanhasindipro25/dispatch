77664c957f621e482e6712e1cb8c68491b54aabb
Aryan Rahman <57558971+Aryan3212@users.noreply.github.com>
2024-02-27 01:08:29 +0600

    chore: cleaned up code

--- app/public/components/ui/MonthlyTimeline.tsx
+++ app/public/components/ui/MonthlyTimeline.tsx
@@ -1,8 +1,6 @@
 "use client";
 import { api } from "../../../../convex/_generated/api";
 import { usePaginatedQuery } from "convex/react";
-import { generateActivityLogMsg } from "../../../common/helpers/UtilsKit";
-import monthlyTimelineData from "../../../common/staticData/monthlyTimelineData";
 import React from "react";
 
 export default function MonthlyTimeline() {
@@ -41,7 +39,7 @@ export default function MonthlyTimeline() {
           </div>
         ))}
         
-        <button onClick={() => loadMore(5)} disabled={status !== "CanLoadMore"}>
+        <button type="button" className="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700" onClick={() => loadMore(5)} disabled={status !== "CanLoadMore"}>
           Load More
         </button>
       </div>
