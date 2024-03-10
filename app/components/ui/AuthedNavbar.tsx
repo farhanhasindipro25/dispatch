298f4ae59b4f51b83aae93b6d637751287ee57d4
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-03-10 22:25:36 +0600

    Added static left side bar

---
+++ app/components/ui/AuthedNavbar.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+export default function AuthedNavbar() {
+  return (
+    <div className="bg-neutral-950 border-b border-neutral-800">
+      <div className="p-4 max-w-9xl mx-auto flex justify-between items-center gap-4">
+        <Link className="flex items-center gap-2" href="/dashboard">
+          <Image
+            src="/logo/alt logo.svg"
+            width={2160}
+            height={1080}
+            alt="RGC Logo"
+            className="cursor-pointer h-5 w-5"
+          />
+          <h2 className="font-semibold italic text-sm text-gray-300">
+            DISPATCH
+          </h2>
+        </Link>
+        <Image
+          src="/images/user.jpg"
+          width={2160}
+          height={1080}
+          alt="RGC Logo"
+          className="cursor-pointer aspect-auto object-cover rounded-full h-10 w-10"
+        />
+      </div>
+    </div>
+  );
+}
--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { PageLayoutProps } from "../interfaces/layouts/PageLayoutProps";
+import LeftSideNavigation from "../components/ui/LeftSideNavigation";
+import AuthedNavbar from "../components/ui/AuthedNavbar";
 
 export default function DashboardLayout({ children }: PageLayoutProps) {
   return (
-    <div className="bg-neutral-950 min-h-screen">
-      <div>{children}</div>
+    <div>
+      <AuthedNavbar />
+      <div className="flex">
+        <LeftSideNavigation />
+        <div className="bg-neutral-950 w-full min-h-screen">{children}</div>
+      </div>
     </div>
   );
 }
