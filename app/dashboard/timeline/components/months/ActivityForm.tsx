5d72749b763490ec8b60afbc61c569bb85640178
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-03-14 14:30:59 +0600

    fix: Fixed build error

--- app/dashboard/timeline/components/months/ActivityForm.tsx
+++ app/dashboard/timeline/components/months/ActivityForm.tsx
@@ -8,16 +8,16 @@ import React, { useState } from "react";
 import { LogType } from "@/types";
 
 export default function ActivityForm({ onSubmit }) {
-  const createLog = useMutation(api.logs.createLog)
+  const createLog = useMutation(api.logs.createLog);
   const tags = useQuery(api.logs.getTags);
   const [selTags, setTags] = useState([]);
-  const [body, setBody] = useState('');
-  const [title, setTitle] = useState('');
-  const [type, setType] = useState('');
+  const [body, setBody] = useState("");
+  const [title, setTitle] = useState("");
+  const [type, setType] = useState("");
   const submitForm = () => {
-    createLog({body, title, type: Number(type), tag_id_list: selTags})
-    onSubmit()
-  }
+    createLog({ body, title, type: Number(type), tag_id_list: selTags });
+    onSubmit();
+  };
 
   return (
     <div className="space-y-4">
@@ -29,9 +29,9 @@ export default function ActivityForm({ onSubmit }) {
         options={tags}
         getOptionLabel={(o) => o.name}
         getOptionValue={(o) => o._id}
-        onChange={e => setTags(e.map((a) => a._id))}
+        onChange={(e) => setTags(e.map((a) => a._id))}
       />
-      <SelectField
+      {/* <SelectField
         label="Type"
         name="type"
         id="type"
@@ -40,13 +40,13 @@ export default function ActivityForm({ onSubmit }) {
           value: k
         }))}
         onChange={e => setType(e.value)}
-      />
+      /> */}
       <TextInputField
         name="topic"
         id="topic"
         label="Topic"
         placeholder="Enter topic"
-        onChange={e => setTitle(e.target.value)}
+        onChange={(e) => setTitle(e.target.value)}
       />
       <AutoSizeTextAreaField
         name="thoughts"
@@ -55,9 +55,13 @@ export default function ActivityForm({ onSubmit }) {
         placeholder="Add your thoughts on the topic"
         minRows={3}
         maxRows={5}
-        onChange={e => setBody(e.target.value)}
+        onChange={(e) => setBody(e.target.value)}
       />
-      <Button variant="ACCENT" className="w-full justify-center" onClick={submitForm}>
+      <Button
+        variant="ACCENT"
+        className="w-full justify-center"
+        onClick={submitForm}
+      >
         Add
       </Button>
     </div>
