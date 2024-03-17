efa3c50315b444df2ca661ec3195ba9a60a09b66
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-03-17 13:59:39 +0600

    fix: Fixed public route protection issues

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,9 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
+
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/public/(.*)"],
 });
- 
+
 export const config = {
   matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
